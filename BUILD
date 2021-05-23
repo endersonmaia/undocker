966557430824973c496a787762b657635ec1a954
Motiejus Jakštys <motiejus@jakstys.lt>
2021-05-24 00:11:58 +0300

    wip leave only rootfs

--- BUILD
+++ BUILD
@@ -1,4 +1,4 @@
-load("@io_bazel_rules_go//go:def.bzl", "go_binary", "go_library")
+load("@io_bazel_rules_go//go:def.bzl", "go_binary", "go_library", "go_test")
 load("@bazel_tools//tools/build_defs/pkg:pkg.bzl", "pkg_tar")
 load("@io_bazel_rules_docker//container:container.bzl", "container_bundle")
 
@@ -8,9 +8,7 @@ go_library(
     importpath = "git.sr.ht/~motiejus/code/undocker",
     visibility = ["//visibility:private"],
     deps = [
-        "//src/undocker/internal/cmdlxcconfig:go_default_library",
-        "//src/undocker/internal/cmdmanpage:go_default_library",
-        "//src/undocker/internal/cmdrootfs:go_default_library",
+        "//src/undocker/rootfs:go_default_library",
         "@com_github_jessevdk_go_flags//:go_default_library",
     ],
 )
@@ -19,4 +17,15 @@ go_binary(
     name = "undocker",
     embed = [":go_default_library"],
     visibility = ["//visibility:public"],
+)
+
+go_test(
+    name = "go_default_test",
+    srcs = ["main_test.go"],
+    embed = [":go_default_library"],
+    deps = [
+        "@com_github_jessevdk_go_flags//:go_default_library",
+        "@com_github_stretchr_testify//assert:go_default_library",
+        "@com_github_stretchr_testify//require:go_default_library",
+    ],
 )
--- main.go
+++ main.go
@@ -1,28 +1,78 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"os"
 
+	"git.sr.ht/~motiejus/code/undocker/rootfs"
 	goflags "github.com/jessevdk/go-flags"
-	"git.sr.ht/~motiejus/code/undocker/internal/cmdlxcconfig"
-	"git.sr.ht/~motiejus/code/undocker/internal/cmdmanpage"
-	"git.sr.ht/~motiejus/code/undocker/internal/cmdrootfs"
 )
 
+const _description = "Flatten a docker container image to a tarball"
+
 func main() {
-	parser := goflags.NewParser(nil, goflags.Default)
-
-	rootfs := cmdrootfs.NewCommand()
-	lxcconfig := cmdlxcconfig.NewCommand()
-	manpage := cmdmanpage.NewCommand(parser)
-	parser.AddCommand("rootfs", rootfs.ShortDesc(), rootfs.LongDesc(), rootfs)
-	parser.AddCommand("lxcconfig", lxcconfig.ShortDesc(), lxcconfig.LongDesc(), lxcconfig)
-	m, _ := parser.AddCommand("man-page", "", "", manpage)
-	m.Hidden = true
-
+	parser := goflags.NewParser(newCommand(), goflags.Default)
 	_, err := parser.Parse()
 	if err != nil {
 		os.Exit(1)
 	}
 	os.Exit(0)
+}
+
+// command implements go-flags.Command
+type command struct {
+	flattener func(io.ReadSeeker, io.Writer) error
+	Stdout    io.Writer
+
+	PositionalArgs struct {
+		Infile  goflags.Filename `long:"infile" description:"Input tarball"`
+		Outfile string           `long:"outfile" description:"Output path, stdout is '-'"`
+	} `positional-args:"yes" required:"yes"`
+}
+
+// newCommand creates a new Command struct
+func newCommand() *command {
+	return &command{
+		flattener: rootfs.Flatten,
+		Stdout:    os.Stdout,
+	}
+}
+
+// Execute executes rootfs Command
+func (c *command) Execute(args []string) (err error) {
+	if len(args) != 0 {
+		return errors.New("too many args")
+	}
+
+	rd, err := os.Open(string(c.PositionalArgs.Infile))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err1 := rd.Close()
+		if err == nil {
+			err = err1
+		}
+	}()
+
+	var out io.Writer
+	if fname := string(c.PositionalArgs.Outfile); fname == "-" {
+		out = c.Stdout
+	} else {
+		outf, err := os.Create(fname)
+		if err != nil {
+			return fmt.Errorf("create: %w", err)
+		}
+		defer func() {
+			err1 := outf.Close()
+			if err == nil {
+				err = err1
+			}
+		}()
+		out = outf
+	}
+
+	return c.flattener(rd, out)
 }
